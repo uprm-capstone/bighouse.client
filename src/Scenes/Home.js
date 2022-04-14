bfa2c6b11149f4a2ff609b823a2c829858d6f878
Jomar Santos <jomar.santos@upr.edu>
2022-04-14 15:44:34 -0400

    Home require axios and request testing.

--- src/Scenes/Home.js
+++ src/Scenes/Home.js
@@ -10,68 +10,60 @@ export default function Home(){
 
     /*Test User*/
     const [user, setUser] = useState(User.user)
+    const [userName, setUserName] = useState('')
     const [document, setDocument] = useState(User.document)
     const [apartmentCost, setApartmentCost] = useState('')
     const [issue, setIssue] = useState(User.issue) 
     const [utility, setUtility] = useState(User.utility)
     const [utilityCost, setUtilityCost] = useState('')
     const [totalCost, setTotalCost] = useState(apartmentCost+utilityCost)
+    const [lastPayment, setLastPayment] = useState(0)
+    const [balanaceComp, setBalanaceComp] = useState(0)
+    const [balance, setBalannce] = useState('')
 
+    const [year, setYear] = useState(new Date().getFullYear())
 
+    const nMonth=()=>{
+        let holder = new Date().getMonth();
+        if(holder>=12){
+            setYear(new Date().getFullYear()+1);
+            console.log(year);
+            return 1;
+        }
+        else{
+            console.log(year);
+            return new Date().getMonth()+2;
+        }
+    }
 
-/*Axios for name, payment info, utilities info, etc. */
+    const [month, setMonth] = useState(nMonth)
+    
 
-    // const api = axios.create({
-    //     baseURL: 'http://localhost:8008/'
-    //  })
+    
 
-    // /*Get User*/
-    // api.get('users/user').then(res => {
-    //       setUser(res)
-    // })
-
-    // /*Get Payment Balance MISSING PAYMENT DUE DATE, AND REPORTED ON DATE*/
-    // api.get('apartments/apartment-cost').then(res => {
-    //     setApartmentCost(res)
-    // })
-
-    // /*Get Issues*/
-    // api.get('issues/').then(res => {
-    //     setIssue(res)
-    // })
-
-    // /*Get Utility*/
-    // api.get('utility/get-apartment-utilities').then(res => {
-    //     setUtility(res)
-    // })
-
-    // /*Get Utility Cost*/
-    // api.get('utility/get-utility-total').then(res => {
-    //     setUtilityCost(res)
-    // })
-
-
-    // /*Get Documents*/
-    // api.get('documents/').then(res => {
-    //     setDocument(res)
-    // })
 
     useEffect(() => {
         console.log("Entered UseEffect:")
         const apartment = {
             apartment_id:3
         };
-        // axios.get(`http://localhost:8008/apartments/apartment-total-cost`, apartment)
-        // .then(res => {
-        //     console.log("RESULT:")
-        //     console.log(res);
-        //     setApartmentCost(res)
-        //     console.log(apartmentCost)
-        // })
-        // .catch((error) => {
-        //     console.log(error);
-        // })
 
+        // Gets user name
+        axios({
+            method: 'GET',
+            params: {user_id:3},
+            url: `http://localhost:8008/users/user`
+        })
+        .then(res => {
+                console.log("PAYMENT:")
+                console.log(res.data);
+                setUserName(res.data.user_name);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+        //Gets apartment cost
         axios({
             method: 'GET',
             params: apartment,
@@ -86,6 +78,8 @@ export default function Home(){
             .catch((error) => {
                 console.log(error);
             })
+
+            //Gets utility cost
             axios({
                 method: 'GET',
                 params: apartment,
@@ -103,14 +97,41 @@ export default function Home(){
             var holder = parseFloat(apartmentCost+utilityCost).toFixed(2);
             console.log("TOTAL COST: "+holder);
             setTotalCost(holder);
+
+            // Get payment data
+            axios({
+                method: 'GET',
+                params: {user_id:3},
+                url: `http://localhost:8008/payments/get-payment-user`
+            })
+            .then(res => {
+                    console.log("PAYMENT:")
+                    console.log(res.data);
+                    setLastPayment(res.data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+
+            if(parseFloat(parseFloat(utilityCost)/parseFloat(lastPayment))>1){
+                setBalanaceComp(parseFloat(parseFloat(utilityCost)/parseFloat(lastPayment.utility_cost))-1);
+                console.log("Balance is: "+balanaceComp);
+                setBalannce("statusMarker");
+            }
+            else{
+                setBalanaceComp(parseFloat(1-parseFloat(utilityCost)/parseFloat(lastPayment.utility_cost)));
+                console.log("Balance is: "+balanaceComp);
+                setBalannce("balanceMarker");
+            }
+
+
     })
 
     return(
         <section class="HomeSection"> 
-        
         <Nav />
-        <h1 class="homeGray">Hi {user.firstName}!</h1>
-        <p class="homeGray"> Your next payment is scheduled for {user.paymentDueDate} </p>
+        <h1 class="homeGray">Hi {userName}!</h1>
+        <p class="homeGray"> Your next payment is scheduled for {month}/1/{year} </p>
 
 
         <div class="paymentBalanceBlock">
@@ -123,14 +144,14 @@ export default function Home(){
             <label class="blockTitle"> Utilities Balance</label> <br />
             <div className="subBlock"> 
             <label class="blockInfo"> {parseFloat(utilityCost).toFixed(2)} </label>
-            <label class="balanceMarker">{utility.utilityBalanceMarker}</label>
+            <label class={balance}>{balanaceComp.toFixed(2)+"^"}</label>
             </div>
         </div>
 
         <div class="documentsBlock">
-            <label class="blockTitle"> Last Payment {document.documentReportDate}</label> <br />
+            <label class="blockTitle"> Last Payment {lastPayment.payment_date}</label> <br />
             <div className="subBlock" > 
-            <label class="blockInfo"> {document.documentInfo} </label>
+            <label class="blockInfo"> ${lastPayment.total} </label>
             <Button name="More" class="moreButton" />
             </div>
         </div>
