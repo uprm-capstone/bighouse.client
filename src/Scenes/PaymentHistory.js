6c179a38118bfef928df1c6c04e4117ad0827049
Gustavo <grfroman@Andros-PC.localdomain>
2022-04-24 17:38:36 -0400

    Create Payment-History scene.

--- src/Scenes/PaymentHistory.js
+++ src/Scenes/PaymentHistory.js
@@ -1,35 +1,35 @@
 import React from 'react';
 import axios from 'axios'; 
-import PaymentHistory from '../Components/Tests/PaymentHistoryScene.json'; 
+import PaymentHistoryScene from '../Components/Tests/PaymentHistoryScene.json'; 
 import {useRef, useState, useEffect} from 'react';
 import '../Styles/index.css';
 import Button from '../Components/Buttons/Button.js';
 import Nav from '../Components/Sections/Nav.js'; 
 
 export default function PaymentHistory(){
-
-        
-    const [history, setDocument] = useState(PaymentHistory.payment)
-
-
+     
+    const [payment, setPayments] = useState(PaymentHistoryScene.payment)
 
 /*Axios*/
 
     const functionHandler = () => {
-      
+
     }
 
     return(
+
         <section class="paymentHistorySection"> 
         
-        <Nav />
-        <h1 class="documentHeader"> Payment History </h1>
+        <div> <Nav /> </div>
+        <h1 class="paymentHistoryHeader"> Payment History </h1>
+        <h1 class="paymentHeader"> Payments </h1>
 
-        {history.map(history => (
+        {payment.map(payment => (
         <div class="payment">
-            <label class="blockTitle"> Created on {history.creation}</label> <br />
-            <label class="blockInfo"> {history.title} </label> <br />
-            <Button name="More" onClick={functionHandler} class="moreButton" />
+            <label class="blockTitle"> Payment sent on {payment.creation}</label> <br />
+            <label class="blockInfo"> ${payment.total} </label>
+            <Button name="More" onClick={functionHandler} class="morePayment" /> 
+           
         </div>
         ))}
 
